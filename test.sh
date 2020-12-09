5e4ba6c9de0558ef3ce0983bdac3d8d033129052
yuina-blend <moon.h14.dd2.as@gmail.com>
2020-12-09 11:21:01 +0900

    接続しているSSIDと設定の判定できた

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+http_proxy="http://hoge.jp"
+https_proxy="https://hoge.jp"
+wireless_network_interface_name="wlp1s0"
+SSID="ibakosen"
 
-NAME=`iwconfig wlp1s0 | grep ESSID`
-echo ${NAME##*ESSID:}
+NAME=`iwconfig | grep ${wireless_network_interface_name}`
+
+if [ `echo ${NAME##*ESSID:} | grep ${SSID}` ];then
+    echo OK
+else
+    echo NO
+fi
